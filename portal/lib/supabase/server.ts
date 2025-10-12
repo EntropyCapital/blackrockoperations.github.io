75446df07df0a0f39011e17b14f3451b994bc18b
Blackrock operations <coltonletellier23@gmail.com>
2025-10-11 18:47:37 -0700

    Update server.ts

--- portal/lib/supabase/server.ts
+++ portal/lib/supabase/server.ts
@@ -6,7 +6,7 @@ import { cookies } from "next/headers"
  * global variable. Always create a new client within each function when using
  * it.
  */
-export async function createClient() {
+export function createClient() {
   const cookieStore = await cookies()
 
   return createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
